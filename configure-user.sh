7b8916f736c4390d2776215ba828cf8a82378933
1nt18is076 <1nt18is076.kaushal@nmit.ac.in>
2021-11-20 15:38:45 +0530

    add python crypto

--- configure-user.sh
+++ configure-user.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git config user.name "Kaushal"
+git config user.name "1nt18is076"
 git config user.email "1nt18is076.kaushal@nmit.ac.in"
 
 git config credential.helper 'cache --timeout=10'
